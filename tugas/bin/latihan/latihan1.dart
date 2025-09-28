d7e03d9c57bfa4e32eedfda46626970024c881c1
ameliaolaa <ameliasyaifuddin@gmail.com>
2025-09-28 21:21:56 +0700

    tambahkan folder latihan dan tugas

---
+++ tugas/bin/latihan/latihan1.dart
@@ -0,0 +1,10 @@
+void main(){
+  String nama = "Lola Amelia"; 
+  int umur = 19; 
+  double tinggiBadan = 156; 
+  bool isActive = true;
+print ("Nama: $nama");
+print ("Umur: $umur");
+print ("Tinggi: $tinggiBadan");
+print("Status: ${isActive ? 'Aktif' : 'Tidak Aktif'}");
+}
